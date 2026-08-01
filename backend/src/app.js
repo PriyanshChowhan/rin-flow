fd5c0cd99bce3c58255c5293f22e86b31bab1793
Priyansh Chowhan <priyanshchowhan@gmail.com>
2026-08-01 23:54:23 +0530

    fix: update CORS configuration to use specific origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://your-frontend-domain.onrender.com",
-];
-
 app.use(cors({
-    origin: allowedOrigins,
+    origin: "https://rin-flow.onrender.com",
     credentials: true
 }))
+
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
